88182ecff1014978834747f415687fc39ee0f724
h-xDF <raralux@yandex.ru>
2019-10-06 21:51:59 +0300

    odometer
    location listener

--- odometer/src/main/java/com/hfad/odometer/MainActivity.java
+++ odometer/src/main/java/com/hfad/odometer/MainActivity.java
@@ -1,11 +1,18 @@
 package com.hfad.odometer;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -15,6 +22,8 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private OdometerService odometer;
     private boolean bound = false;
+    private final int PERMISSION_REQUEST_CODE = 698;
+    private final int NOTIFICATION_ID = 423;
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
@@ -42,9 +51,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION_STRING)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {OdometerService.PERMISSION_STRING},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            bindOdometerService();
+        }
     }
 
     @Override
@@ -56,6 +70,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    bindOdometerService();
+                } else {
+                    //Create a notification builder
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle(getResources().getString(R.string.app_name))
+                            .setContentText(getResources().getString(R.string.permission_denied))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[] {1000, 1000})
+                            .setAutoCancel(true);
+                    //Created action
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                            this,
+                            0,
+                            actionIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(actionPendingIntent);
+                    //Notification
+                    NotificationManager notificationManager =
+                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+                }
+        }
+    }
+
     private void displayDistance() {
         final TextView distanceView = findViewById(R.id.distance);
         final Handler handler =  new Handler();
@@ -64,15 +111,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 double distance = 0.0;
-                if (bound & odometer != null) {
+                if (bound && odometer != null) {
                     distance = odometer.getDistance();
                 }
 
                 String distanceStr = String.format(Locale.getDefault(),
-                        "%1$, .2f km", distance);
+                        "%1$, .2f meters", distance);
                 distanceView.setText(distanceStr);
                 handler.postDelayed(this, 1000);
             }
         });
+    }
+
+    private void bindOdometerService() {
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 }
